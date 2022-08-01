f373c9e6689c43efbfb16233155d1f14904a0f23
borangoher <goherboran1337@gmail.com>
2022-08-01 17:44:43 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import uniqid from "uniqid";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor () {
+        super();
+
+        this.state = {
+            name: "",
+            surname: "",
+            location: "",
+            university: "",
+            degree: "",
+            gradYear: "",
+            previousEmployment: [],
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            name: e.target.parentElement.children[1].value,
+            surname: e.target.parentElement.children[3].value,
+            location: e.target.parentElement.children[5].value,
+            university: e.target.parentElement.children[7].value,
+            degree: e.target.parentElement.children[9].value,
+            gradYear: e.target.parentElement.children[11].value,
+        
+        })
+    }
+
+    handleAdd = (e) => {
+      e.preventDefault();
+      this.setState({
+            previousEmployment: this.state.previousEmployment.concat([{
+              employer: e.target.parentElement.children[1].value,
+              startYear: e.target.parentElement.children[3].value,
+              endYear: e.target.parentElement.children[5].value,
+              role: e.target.parentElement.children[7].value,
+              id: uniqid(),
+          }])
+      })
+    }
+
+    render() {
+      return(
+        <div>
+          <form onSubmit={this.pass}>
+            <label htmlFor="name">Name</label>
+            <input name="name" type="text" onChange={this.handleChange}></input>
+            <label htmlFor="surname">Surname</label>
+            <input name="surname" type="text" onChange={this.handleChange}></input>
+            <label htmlFor="location">Location</label>
+            <input name="location" type="text" onChange={this.handleChange}></input>
+            <label htmlFor="university">University</label>
+            <input name="university" type="text" onChange={this.handleChange}></input>
+            <label htmlFor="degree">Degree</label>
+            <input name="degree" type="text" onChange={this.handleChange}></input>
+            <label htmlFor="gradYear">Graduation Year</label>
+            <input name="gradYear" type="text" onChange={this.handleChange}></input>
+          </form>
+          <form>
+            <label htmlFor="employer">Previous Employer</label>
+            <input name="employer" type="text"></input>
+            <label htmlFor="startYear">Start Year</label>
+            <input name="startYear" type="text"></input>
+            <label htmlFor="endYear">End Year</label>
+            <input name="endYear" type="text"></input>
+            <label htmlFor="role">Role</label>
+            <input name="role" type="text"></input>
+            <button onClick={this.handleAdd}>Add</button>
+          </form>
+        </div>
+      );
+    }
 }
 
 export default App;
